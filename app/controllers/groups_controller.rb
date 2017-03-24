c5b08a440974878976eefbe38305b11736e6440a
amy326 <amy326@163.com>
2017-03-24 23:50:21 +0800

    add permission check on show page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -28,6 +28,7 @@ class GroupsController < ApplicationController
       render :new
     end
   end
+
   def update
       if @group.update(group_params)
         redirect_to groups_path, notice: "Update Success"
@@ -41,8 +42,6 @@ class GroupsController < ApplicationController
     redirect_to groups_path, alert: "Group deleted"
   end
 
-
-
   private
 
   def find_group_and_check_permission
@@ -51,9 +50,9 @@ class GroupsController < ApplicationController
     if current_user != @group.user
       redirect_to root_path, alert:"You have no permission."
     end
+  end
 
   def group_params
     params.require(:group).permit(:title, :description)
   end
-
 end
