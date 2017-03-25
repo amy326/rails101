7604ea392b3a663c66e3fd7f22255bdb621cbbd4
amy326 <amy326@163.com>
2017-03-25 21:08:36 +0800

    implement group relation

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,8 @@ class Group < ApplicationRecord
   has_many :group_relationships
   has_many :members, through: :group_relationships, source: :user
   validates :title, presence: true
+
+has_many :group_relationships
+has_many :members, through: :group_relationships, source: :user
+
 end
